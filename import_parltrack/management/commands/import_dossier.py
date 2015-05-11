57affc6f980ff0b6f4090518ba38ff6570ec2cc1
Arnaud Fabre <arnaud.fabre@camobscura.fr>
2015-05-11 15:11:54 +0200

    fix import

--- import_parltrack/management/commands/import_dossier.py
+++ import_parltrack/management/commands/import_dossier.py
@@ -3,24 +3,63 @@
 import urllib
 import json
 
+from datetime import datetime
+
 from os.path import join
 from slugify import slugify
-from pprint import pprint
 
 from django.core.management.base import BaseCommand
-
 from representatives_votes.models import Dossier, Proposal, Vote
-from representatives.models import Representative
-import parltrack_votes.utils as utils
+# from representatives.models import Representative
+import import_parltrack.utils as utils
 
 PARLTRACK_URL = 'http://parltrack.euwiki.org/dossier/%s?format=json'
 
+_parse_date = lambda date: datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
 
 class Command(BaseCommand):
-
+    """
+    Import a Dossier from parltrack and save it in the
+    representatives_votes model format
+    """
     def add_arguments(self, parser):
         parser.add_argument('dossier_id', nargs=1, type=str)
 
+    def parse_json_dossier_data(self, dossier_data):
+        
+        dossier, created = Dossier.objects.get_or_create(reference=dossier_data['procedure']['reference'])
+        dossier.title = dossier_data['procedure']['title']
+        dossier.link = dossier_data['meta']['source'] 
+        dossier.save()
+
+        Vote.objects.filter(proposal__dossier=dossier).delete()
+        Proposal.objects.filter(dossier=dossier).delete()
+        
+        for proposal_data in dossier_data['votes']:
+            proposal = Proposal.objects.create(
+                dossier=dossier,
+                title=proposal_data['title'],
+                reference=proposal_data['report'],
+                datetime=_parse_date(proposal_data['ts']),
+                total_for=int(proposal_data['For']['total']),
+                total_abstain=int(proposal_data['Abstain']['total']),
+                total_against=int(proposal_data['Against']['total'])
+            )
+
+            positions = ['For', 'Abstain', 'Against']
+            for position in positions:
+                for group_vote_data in proposal_data[position]['groups']:
+                    group_name = group_vote_data['group']
+                    for vote_data in group_vote_data['votes']:
+                        for mep in vote_data:                        
+                            representative = utils.find_matching_representatives_in_db(
+                                vote_data['orig'], proposal.datetime, group_name)
+                            Vote.objects.create(
+                                proposal=proposal,
+                                representative_remote_id=representative.remote_id,
+                                position=position.lower()
+                            )
+                            
     def handle(self, *args, **options):
         dossier_id = unicode(args[0])        
         parltrack_url = PARLTRACK_URL % dossier_id
@@ -31,42 +70,4 @@ class Command(BaseCommand):
         urllib.urlretrieve(parltrack_url, json_dump_localization)
 
         dossier_data = json.load(open(json_dump_localization))
-
-        dossier, created = Dossier.objects.get_or_create(reference=dossier_data['procedure']['reference'])
-
-        dossier.title = dossier_data['procedure']['title']
-        dossier.link = dossier_data['meta']['source']
-
-        dossier.save()
-
-        for proposal_data in dossier_data['votes']:
-            proposal = Proposal()
-            proposal.dossier = dossier
-            proposal.title = proposal_data['title']
-            proposal.reference = proposal_data['report']
-            proposal.date = proposal_data['ts']
-
-            proposal.total_for = int(proposal_data['For']['total'])
-            proposal.total_abstain = int(proposal_data['Abstain']['total'])
-            proposal.total_against = int(proposal_data['Against']['total'])
-            
-            print(proposal.__dict__)
-            
-            for group_vote_data in proposal_data['For']['groups']:
-                group_name = group_vote_data['group']
-                print(group_name)
-                for vote_data in group_vote_data['votes']:
-                    for mep in vote_data:
-                        print(vote_data['orig'])
-                        
-                
-            # print(proposal.__dict__)
-            # print(proposal_data.keys())
-            # print(proposal_data['For'])
-
-        # utils.find_matching_mep_in_db('test', 'test 2')
-        # print(dossier['votes'])
-        
-        # pprint(dossier['votes'])
-        # pprint(dossier['meta'])
-        # pprint(dossier['procedure'])
+        self.parse_json_dossier_data(dossier_data)
--- import_parltrack/utils.py
+++ import_parltrack/utils.py
@@ -1,32 +1,56 @@
 # coding: utf-8
 
+"""
+Fix for ugly data from Parltrack
+"""
 
+import re
+import json
 
-def find_matching_mep_in_db(mep, vote_date, mep_group):
-    def mep_filter(**args):
-        return  [cm.mep for cm in CountryMEP.objects.select_related('mep').filter(begin__lt=vote_date, end__gt=vote_date, **args)]
+from urllib import urlopen
+# from slugify import slugify
 
+from representatives.models import Mandate, Representative
+
+def find_matching_representatives_in_db(mep, vote_date, representative_group):
+    # Only select representatives that are on mandate during the vote
+
+    def representative_filter(**args):
+        mandates = Mandate.objects.select_related('representative').filter(
+            group__kind='country',
+            begin_date__lte=vote_date,
+            end_date__gte=vote_date,
+            **args
+        )
+        
+        return [mandate.representative for mandate in mandates]
+    
     mep = mep.replace(u"ß", "SS")
     mep = mep.replace("(The Earl of) ", "")
 
-    representative = mep_filter(mep__last_name=mep)
+    representative = representative_filter(representative__last_name__iexact=mep)
     if not representative:
-        representative = mep_filter(mep__last_name__iexact=mep)
+        representative = representative_filter(representative__last_name__iexact=re.sub("^DE ", "", mep.upper()))
     if not representative:
-        representative = mep_filter(mep__last_name__iexact=re.sub("^DE ", "", mep.upper()))
+        representative = representative_filter(representative__last_name__contains=mep.upper())
     if not representative:
-        representative = mep_filter(mep__last_name__contains=mep.upper())
+        representative = representative_filter(representative__full_name__contains=re.sub("^MC", "Mc", mep.upper()))
     if not representative:
-        representative = mep_filter(mep__full_name__contains=re.sub("^MC", "Mc", mep.upper()))
-    if not representative:
-        representative = mep_filter(mep__full_name__icontains=mep)
+        representative = representative_filter(representative__full_name__icontains=mep)
+    # if not representative:
+        # representative = representative_filter(representative__slug__endswith=slugify(mep))
 
     if representative:
-        return representative[0]
-
-    print "WARNING: failed to get mep using internal db, fall back on parltrack"
+        # TODO Ugly hack, we should handle cases where there are multiple results
+        representative = representative[0]
+        return representative
+    
+    print 'WARNING: failed to get mep using internal db, fall back on parltrack'
     print "http://parltrack.euwiki.org/mep/%s?format=json" % (mep.encode("Utf-8"))
     mep_ep_id = json.loads(urlopen("http://parltrack.euwiki.org/mep/%s?format=json" % (mep.encode("Utf-8"))).read())["UserID"]
-    print mep_ep_id, mep, json.loads(urlopen("http://parltrack.euwiki.org/mep/%s?format=json" % (mep.encode("Utf-8"))).read())["Name"]["full"]
-    representative = MEP.objects.get(ep_id=mep_ep_id).representative_ptr
+    print mep_ep_id, mep,
+    print json.loads(urlopen("http://parltrack.euwiki.org/mep/%s?format=json" % (mep.encode("Utf-8"))).read())["Name"]["full"]
+
+    representative = Representative.objects.get(remote_id=mep_ep_id)
     return representative
+
