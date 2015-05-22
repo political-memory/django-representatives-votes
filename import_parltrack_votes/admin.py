4b686f213b3ec455da3240ebe2ac086c8d7c58da
Arnaud Fabre <arnaud.fabre@camobscura.fr>
2015-05-22 13:17:30 +0200

    adds admin

---
+++ import_parltrack_votes/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from import_parltrack_votes.models import Matching
+
+class NoneMatchingFilter(admin.SimpleListFilter):
+    title = 'Remote id'
+    parameter_name = 'representative_remote_id'
+
+    def lookups(self, request, model_admin):
+        return [('None', 'Unknown')]
+    
+    def queryset(self, request, queryset):
+        if self.value() == 'None':
+            return queryset.filter(representative_remote_id=None)
+        else:
+            return queryset            
+
+
+class MatchingAdmin(admin.ModelAdmin):
+    model = Matching
+    list_display = ('mep_name', 'mep_group', 'representative_remote_id')
+    list_filter = (NoneMatchingFilter,)
+    
+admin.site.register(Matching, MatchingAdmin)
