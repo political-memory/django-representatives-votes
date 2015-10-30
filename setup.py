2f09f6045b81485018474962a09ee5ad2ff41aed
jpic <jamespic@gmail.com>
2015-10-30 21:20:58 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='import-parltrack-votes',
+    version='0.0.1',
+    description='Base app for importing parltrack votes',
+    author='Arnaud Fabre',
+    author_email='webmaster@memopol.org',
+    url='http://github.com/political-memory/toutatis-import_parltrack',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GPLv3',
+    keywords='django government parliament',
+    classifiers=[
+        'Development Status :: 1 - Alpha/Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
