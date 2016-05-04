3c107a377b77523f7ad82960513c18ab9aabeb75
jpic <jamespic@gmail.com>
2016-05-04 22:41:29 +0200

    Fix Dossier list API queryset

--- representatives_votes/api.py
+++ representatives_votes/api.py
@@ -26,7 +26,7 @@ class DossierViewSet(viewsets.ReadOnlyModelViewSet):
     """
 
     pagination_class = DefaultWebPagination
-    queryset = Dossier.objects.all()
+    queryset = Dossier.objects.prefetch_related('proposals__votes__representative')
     serializer_class = DossierSerializer
 
     filter_backends = (
